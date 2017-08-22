7dba2a93e350b9f34f0adda2274e1e95b49f0255
Tony Lee <tlee@lfl.hk>
2017-08-22 10:36:07 +0800

    updated for running dev component page

--- packages/react-scripts/template/src/MyComponent/index.tsx
+++ packages/react-scripts/template/src/MyComponent/index.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 
-
 export interface MyComponentProps {
     title: string;
 }
 
-
 export interface MyComponentState {
 
 }
-
 
 export class MyComponent extends React.Component<MyComponentProps, MyComponentState> {
     render() {
--- packages/react-scripts/template/src/dev.tsx
+++ packages/react-scripts/template/src/dev.tsx
@@ -1,5 +1,6 @@
+import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { MyComponent } from './MyComponent/index';
+import { MyComponent } from './index';
 
 ReactDOM.render(
     <MyComponent title="Hello World!" />,
