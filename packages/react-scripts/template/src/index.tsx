bb67efaef9c3965d87b775c96f4bfe7d572a218a
Rafael Staib <me@rafaelstaib.com>
2017-12-27 15:02:27 +0100

    Removed unnecessary import (index.css) and increased patch version (2.8.4)

--- packages/react-scripts/template/src/index.tsx
+++ packages/react-scripts/template/src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 ReactDOM.render(
   <App />,
