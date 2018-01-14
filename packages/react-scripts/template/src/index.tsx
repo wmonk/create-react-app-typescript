0d769f9f68d850d098fb824713c956b1f0472817
Alain Jacomet <alain.jacomet.forte@gmail.com>
2018-01-14 10:07:16 -0300

    v0.0.1: SASS, External SW, Package name, Template

--- packages/react-scripts/template/src/index.tsx
+++ packages/react-scripts/template/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
   <App />,
