9f5a5ce84d4de8221de6bfd1cb083d09c22d5ade
Wang Guan <momocraft@gmail.com>
2018-04-21 09:45:53 +0900

    enable webpack hmr

--- packages/react-scripts/template/src/index.tsx
+++ packages/react-scripts/template/src/index.tsx
@@ -4,8 +4,26 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+function registerHMR() {
+  type ModuleHMR = typeof module & {
+    hot?: {
+      accept(dependencies: string | string[], callback: (updatedDependencies: any[]) => void): void
+    }
+  };
+
+  if ((module as ModuleHMR).hot) {
+    (module as ModuleHMR).hot!.accept('./App', () => {
+      ReactDOM.render(
+        <App />,
+        document.getElementById('root') as HTMLElement
+      );
+    });
+  }
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
+registerHMR();
