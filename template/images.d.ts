8849c5d2ecf69bab8b6eb962b7608ce98a63c4e7
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2018-10-07 22:57:05 +0800

    fix: css-module support about '@keyframes logo-spin'

--- template/images.d.ts
+++ template/images.d.ts
@@ -5,6 +5,19 @@ declare module '*.jpeg'
 declare module '*.gif'
 declare module '*.bmp'
 declare module '*.tiff'
+
+declare module '*.module.css' {
+  const exports: { [exportName: string]: string };
+  export = exports;
+}
+declare module '*.module.scss' {
+  const exports: { [exportName: string]: string };
+  export = exports;
+}
+declare module '*.module.sass' {
+  const exports: { [exportName: string]: string };
+  export = exports;
+}
 declare module '*.css'
 declare module '*.scss'
 declare module '*.sass'
