72ad68235b5b9e2d5b48888c80ce23c918655fe3
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2018-10-23 15:02:45 +0800

    add shims of style

--- template/shims/styles.d.ts
+++ template/shims/styles.d.ts
@@ -1,10 +1,3 @@
-declare module '*.svg'
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.jpeg'
-declare module '*.gif'
-declare module '*.bmp'
-declare module '*.tiff'
 
 declare module '*.module.css' {
   const exports: { [exportName: string]: string };
